b4c2e54b3d0c39150afabe5563d39b0ae5337ba5
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-09-08 18:53:57 -0700

    coded

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,7 +1,10 @@
 require ( './root.js' );
 
 var a, b;
-
+beforeEach(function () {
+  a = Math.floor(Math.random() * 1000);
+  b = Math.floor(Math.random() * 1000);
+});
 
 function add(a, b) {
   return a + b;
