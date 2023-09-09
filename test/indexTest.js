61c6d4e7d99b04a2954e10e9bb888334c33ff963
Dylynn Barrows <DylynnBarrows27@gmail.com>
2023-09-08 18:43:02 -0700

    coded

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,11 +1,38 @@
 require ( './root.js' );
 
-var a, b
+function add(a, b) {
+  return a + b;
+}
 
-beforeEach(function() {
-  a = Math.floor(Math.random() * 1000)
-  b = Math.floor(Math.random() * 1000)
-})
+
+function subtract(a, b) {
+  return a - b;
+}   
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function increment(n) {
+  return n +=1
+}
+
+function decrement(n) {
+  return n -=1
+}
+
+function makeInt(string) {
+  return parseInt (string, 10)
+}
+
+function preserveDecimal(string) {
+  return parseFloat (string, 10)
+}
+}
 
 describe('basic math functions', function () {
   it("'add()' is a valid function", function() {
